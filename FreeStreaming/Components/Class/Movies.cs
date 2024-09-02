bf4433ac49fda57af1746ddf5012e3e8c236296d
Sam <157816793+eigth-bit-guy@users.noreply.github.com>
2024-09-02 17:00:05 -0300

    Add files via upload

---
+++ FreeStreaming/Components/Class/Movies.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeStreaming.Components.Class
+{
+    public class Movies
+    {
+        public string? Name { get; set; }
+        public byte[] Thumb { get; set; }
+    }
+}
---
+++ FreeStreaming/Components/Class/Series.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace FreeStreaming.Components.Class
+{
+    public class Series
+    {
+        public string? Name { get; set; }
+        public byte[] Thumb { get; set; }
+        public int Classification { get; set; }
+    }
+}
